7da8be5e87803c047b025cdd08a07d987ac6381c
aaradra779 <aaragyagautam28@gmail.com>
2024-07-11 21:11:58 +0545

    todo checked and delete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,88 @@
-import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, FormBuilder} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, input, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, FormBuilder, NgForm, FormsModule} from '@angular/forms';
+import { find } from 'rxjs';
 
+interface TodoItem {
+  displayedText : string,
+  checked : boolean,
 
+}
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  input = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-  })
+export class HomeComponent implements OnInit {
+
+  inputText: string = '';
+  // displayedText: string | null = null;
+  todoList : TodoItem[] = []
+
+
+  
+  ngOnInit(): void {
+
+    const storedData  = localStorage.getItem('todoList')
+    if(storedData){
+      this.todoList = JSON.parse(storedData)
+    }
+
+    
+  }
+
+  displayText() {
+    // this.displayedText = this.inputText;
+    if (this.inputText!== ''){
+      const newTodoItem : TodoItem = {
+        displayedText: this.inputText,
+        checked : false
+      } 
+      this.todoList.push(newTodoItem)
+      localStorage.setItem('todoList' ,JSON.stringify(this.todoList) )
+      this.inputText = ''
+
+    }
+  }
+
+  @Input({transform: appendPx}) widthPx: number = 0;
+
+
+
+  @Output() eventEmitter = new EventEmitter();
+
+  clickme (){
+    this.eventEmitter.emit(7);
+    // console.log('clicked')  
+    
+  }
+  deleteText(index : number){
+
+    this.todoList.splice(index, 1)
+    localStorage.setItem('todoList' ,JSON.stringify(this.todoList) )
+
+  }
+
+  onCheck(i : number){
+    this.todoList[i].checked = !this.todoList[i].checked
+    localStorage.setItem('todoList' ,JSON.stringify(this.todoList) )
+
+
+
+  }
+
+
 }
+
+
+function appendPx(value: string) {
+  return `${value}px`;
+}
+
+console.log(appendPx('hello'))
+
+
